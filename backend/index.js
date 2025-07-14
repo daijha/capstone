cc092eac8fadc6034a4e6127a73bfc98b4db3666
daijha <daijha.rabalais@gmail.com>
2025-07-14 11:18:44 -0400

    made request from front end to backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 const app = express();
 const port = 3000;
+
+app.use(cors())
 
 
 //base url
 app.get('/', (req,res)=>{
-    res.json("hello world")
+    res.json("hello world backend")
 })
 
 app.listen(port,()=>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
+  // useEffect runs and fetches the JSON from the backend
+  useEffect(() => {
+    async function fetchData() {
+      try{
+ const response = await fetch("http://localhost:3000");
+      const data = await response.json();
+      console.log('fetchData', data)
+      }catch(error){
+        console.log(error)
+      }
      
-    </>
-  )
+    }
+    fetchData()// call the fetch 
+  }, []);
+
+  return <> hello world </>;
+  
 }
 
-export default App
+export default App;
