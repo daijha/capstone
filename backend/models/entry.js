7134c71de638e500ef7ca4a669a933530f8615fc
daijha <daijha.rabalais@gmail.com>
2025-07-14 14:25:53 -0400

    backend:created models folder, schema file, entryschema and its model

---
+++ backend/models/entry.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+// schema syntax : 
+const entrySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 10,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 1000,
+  },
+});
+
+const Entry = mongoose.model("Entry",entrySchema)//  creates model Entry based off of the entrySchema 
