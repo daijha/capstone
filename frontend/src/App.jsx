de1ad1911d9186f44e0084e1b33fd1dc229ea8f1
daijha <daijha.rabalais@gmail.com>
2025-07-14 15:34:47 -0400

    added components to app.jsx .installed react-router-dom+removed strict mode. created react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
 import "./App.css";
 
+import Homepage from "../components/Homepage";
+import CreateEntry from "../components/CreateEntry";
+import ReadEntries from "../components/ReadEntries";
+import EditEntry from "../components/EditEntry";
+import DeleteEntry from "../components/DeleteEntry";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   // useEffect runs and fetches the JSON from the backend
   useEffect(() => {
@@ -17,7 +24,19 @@ function App() {
     fetchData()// call the fetch 
   }, []);
 
-  return <> hello world </>;
+  return <> 
+    <Routes>
+      <Route path = "/" element={<Homepage/>}/>
+      <Route path = "/create" element={<CreateEntry/>}/>
+      <Route path = "/read" element={<ReadEntries/>}/>
+      <Route path = "/edit" element={<EditEntry/>}/>
+      <Route path = "/delete" element={<DeleteEntry/>}/>
+  
+    </Routes>
+
+  
+  
+   </>;
   
 }
 
