bbe48561cf44164ea9e818eb905af25ac75bed01
daijha <daijha.rabalais@gmail.com>
2025-07-15 13:23:31 -0400

    completed crud. tested crud.

--- backend/routes/entries.js
+++ backend/routes/entries.js
@@ -3,21 +3,32 @@ const router = express.Router() // without this i cant have a routes folder... i
 import Entry from "../models/entry.js";// i will have to interact with the model( w/ mongo query language) to access the data for endpoints.  
 
 
-router.get('/', async (req,res)=>{// use router instead of app  when using a router. to read the entries 
-    const readEntries = await Entry.find();
-    console.log("request body", req.body)
+router.get('/', async (req,res)=>{// use router instead of app  when using a router. to read the entries   works
+  const readEntries = await Entry.find();  
     res.status(200).json(readEntries)
 })
 
-router.post('/create', async (req,res)=>{// to make the entry
+router.post('/create', async (req,res)=>{// to make the entry   works
     const newEntry = await Entry.create(req.body);
     res.status(201).json(newEntry)
 })
 
-router.put('/create/:id', async (req,res)=>{// id is needed to edit entry 
+router.get('/entries/:id', async (req,res)=>{// reads one entry    works 
+    const readEntry = await Entry.findById(req.params.id);
+    res.status(200).json(readEntry)
+})
+
+router.put('/entries/:id', async (req,res)=>{// id is needed to edit entry     works
     const editEntry = await Entry.findByIdAndUpdate(req.params.id, req.body,{new:true});
     console.log(req.params.id);
-    res.status(201).json(editEntry)
+    res.status(200).json(editEntry)
+})
+
+
+
+router.delete('/entries/:id', async (req,res)=>{// id is needed to edit entry   works 
+    const deleteEntry = await Entry.findByIdAndDelete(req.params.id);
+    res.status(200).json(deleteEntry)
 })
 
 export default router// export the router not the file name.
