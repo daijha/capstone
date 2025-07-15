4ca2e4f56de7a70254e5118dc6823c2faa26555c
daijha <daijha.rabalais@gmail.com>
2025-07-15 13:49:19 -0400

    added try catch to router crud

--- backend/routes/entries.js
+++ backend/routes/entries.js
@@ -1,37 +1,61 @@
-import express from 'express'// import this anytime i need to use express 
-const router = express.Router() // without this i cant have a routes folder... it needs to be there 
-import Entry from "../models/entry.js";// i will have to interact with the model( w/ mongo query language) to access the data for endpoints.  
+import express from "express"; // import this anytime i need to use express
+const router = express.Router(); // without this i cant have a routes folder... it needs to be there
+import Entry from "../models/entry.js"; // i will have to interact with the model( w/ mongo query language) to access the data for endpoints.
 
+router.get("/", async (req, res) => {
+  // use router instead of app  when using a router. to read the entries
+  try {
+    const readEntries = await Entry.find();
+    res.status(200).json(readEntries);
+  } catch {
+    res.status(500).json({ message: "error!" });
+  }
+});
 
-router.get('/', async (req,res)=>{// use router instead of app  when using a router. to read the entries   works
-  const readEntries = await Entry.find();  
-    res.status(200).json(readEntries)
-})
+router.post("/create", async (req, res) => {
+  // to make the entry
 
-router.post('/create', async (req,res)=>{// to make the entry   works
+  try {
     const newEntry = await Entry.create(req.body);
-    res.status(201).json(newEntry)
-})
+    res.status(201).json(newEntry);
+  } catch {
+    res.status(500).json({ message: "error!" });
+  }
+});
 
-router.get('/entries/:id', async (req,res)=>{// reads one entry    works 
+router.get("/entries/:id", async (req, res) => {
+  // reads one entry
+  try {
     const readEntry = await Entry.findById(req.params.id);
-    res.status(200).json(readEntry)
-})
+    res.status(200).json(readEntry);
+  } catch {
+    res.status(500).json({ message: "error!" });
+  }
+});
 
-router.put('/entries/:id', async (req,res)=>{// id is needed to edit entry     works
-    const editEntry = await Entry.findByIdAndUpdate(req.params.id, req.body,{new:true});
-    console.log(req.params.id);
-    res.status(200).json(editEntry)
-})
+router.put("/entries/:id", async (req, res) => {
+  // id is needed to edit entry
+  try {
+    const editEntry = await Entry.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }); // {new:true} is mongoose syntax that returns the updated document
+    res.status(200).json(editEntry);
+  } catch {
+    res.status(500).json({ message: "error!" });
+  }
+});
 
-
-
-router.delete('/entries/:id', async (req,res)=>{// id is needed to edit entry   works 
+router.delete("/entries/:id", async (req, res) => {
+  // id is needed to edit entry
+  try {
     const deleteEntry = await Entry.findByIdAndDelete(req.params.id);
-    res.status(200).json(deleteEntry)
-})
+    res.status(200).json(deleteEntry);
+  } catch {
+    res.status(500).json({ message: "error!" });
+  }
+});
 
-export default router// export the router not the file name.
+export default router; // export the router not the file name.
 
 /*status codes:
 200  ok   GET
