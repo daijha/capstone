fb1635ef009b97657f64d0abfacf6d76754711c6
daijha <daijha.rabalais@gmail.com>
2025-07-15 14:51:22 -0400

    frontend: created structure for homepage. added links to buttons

--- frontend/components/Homepage.jsx
+++ frontend/components/Homepage.jsx
@@ -1,7 +1,24 @@
-function Homepage(){
-    return(
-        <>Homepage</>
+import { Link } from "react-router-dom";
 
-    )
+function Homepage() {
+  return (
+    <>
+      <div className="bookname">Entries</div>
+      <div className="options">
+        <Link to="/create">
+          <button>create new entry</button>
+        </Link>
+        <Link to ="/edit">
+          <button>edit existing entry</button>
+        </Link>
+        <Link to ="/read">
+          <button> read entries</button>
+        </Link>
+        <Link to= "/delete">
+          <button>delete an entry</button>
+        </Link>
+      </div>
+    </>
+  );
 }
-export default Homepage
+export default Homepage;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,38 +6,40 @@ import CreateEntry from "../components/CreateEntry";
 import ReadEntries from "../components/ReadEntries";
 import EditEntry from "../components/EditEntry";
 import DeleteEntry from "../components/DeleteEntry";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   // useEffect runs and fetches the JSON from the backend
   useEffect(() => {
     async function fetchData() {
-      try{
- const response = await fetch("http://localhost:3000");
-      const data = await response.json();
-      console.log('fetchData', data)
-      }catch(error){
-        console.log(error)
+      try {
+        const response = await fetch("http://localhost:3000");
+        const data = await response.json();
+        console.log("fetchData", data);
+      } catch (error) {
+        console.log(error);
       }
-     
     }
-    fetchData()// call the fetch 
+    fetchData(); // call the fetch
   }, []);
 
-  return <> 
-    <Routes>
-      <Route path = "/" element={<Homepage/>}/>
-      <Route path = "/create" element={<CreateEntry/>}/>
-      <Route path = "/read" element={<ReadEntries/>}/>
-      <Route path = "/edit" element={<EditEntry/>}/>
-      <Route path = "/delete" element={<DeleteEntry/>}/>
-  
-    </Routes>
-
-  
-  
-   </>;
-  
+  return (
+    <>
+      <Routes>
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/create" element={<CreateEntry />} />
+        <Route path="/read" element={<ReadEntries />} />
+        <Route path="/edit" element={<EditEntry />} />
+        <Route path="/delete" element={<DeleteEntry />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
