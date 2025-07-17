b98e0bd1a84dfb3b9bfdf638800c4f94062db90d
daijha <daijha.rabalais@gmail.com>
2025-07-17 01:08:19 -0400

    completed creation of delete page and functionality

--- frontend/components/DeleteEntry.jsx
+++ frontend/components/DeleteEntry.jsx
@@ -1,6 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import {useParams} from 'react-router-dom';
+
 function DeleteEntry(){
+      const navigate = useNavigate()
+    const {id} = useParams();
+
+   async function handleDelete(){
+  
+            try {
+      const response = await fetch(`http://localhost:3000/entries/${id}`, {
+        method: "DELETE",
+    
+      });
+
+      // .ok is a built in success detector for fetch responses.
+      if (response.ok) {
+
+        alert("Entry Deleted.");
+        navigate("/read");// should redirect user back to read page after deletion 
+      } else {
+        alert("Deletion Failed!");
+      }
+    } catch (error) {
+      alert("Error!");
+    }
+
+    }
     return(
-        <>page to display the entry and confirm deletion </>
+        <>
+        <h1> Delete Entry</h1>
+        <p>Are you sure you want to delete this entry?</p>
+        <button onClick={handleDelete}>Confirm Delete </button>
+        </>
 
     )
 }
