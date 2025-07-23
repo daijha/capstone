493c04f21e8f3b3ccb8e940208b1d602bd5f426d
daijha <daijha.rabalais@gmail.com>
2025-07-23 13:45:28 -0400

    added missing styling. fixed edit component url

--- frontend/components/EditEntry.jsx
+++ frontend/components/EditEntry.jsx
@@ -60,7 +60,7 @@ function EditEntry() {
       /* fetch does get requests by default so it needs additional configurations. 
     it also needs to be set to a string  using json stringify sot the browser understands... 
     a header needs to be added with they type of content which is json*/
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/${id}`, {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/entries/${id}`, {
         method: "PUT",
         body: JSON.stringify(editedEntry),
         headers: {
@@ -121,7 +121,7 @@ console.log(editedEntry)
             }}
           > Go Read Entries</button>}
       </div>
-    </>) :( <p> You can't edit this entry. It's not yours.</p>)
+    </>) :( <p className="pageBody"> You can't edit this entry. It's not yours.</p>)
 }</>
   );
 }
